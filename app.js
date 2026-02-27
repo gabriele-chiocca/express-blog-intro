e182df2098ea02a27c0541321b7d220734a8cfba
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-27 14:33:50 +0100

    products object with the post of products

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ app.get('/', (req, res) => {
   res.status(200).json(responseData);
 });
 
+app.get('/bacheca', (req, res) => {
+  const products = [
+    {
+      title: 'Ciambella',
+      Img: 'public\images\ciambellone.jpeg',
+      tags: ['Dolce', 'Ciambella', 'Facile'],
+    },
+    {
+      title: 'Cracked',
+      Img: 'public\images\cracker_barbabietola.jpeg',
+      tags: ['Cracked', 'Barbabietola', 'Intermedio'],
+    },
+    {
+      title: 'Pane fritto',
+      Img: 'public\images\pane_fritto_dolce.jpeg',
+      tags: ['Fritto', 'Pane', 'Difficile'],
+    },
+    {
+      title: 'Pasta barbabietola',
+      Img: 'public\images\pasta_barbabietola.jpeg',
+      tags: ['Pasta', 'Primi piatti', 'Facile'],
+    },
+    {
+      title: 'Torta paeasana',
+      Img: 'public\images\torta_paesana.jpeg',
+      tags: ['Dolce', 'Torta', 'Facile'],
+    },
+  ];
+
+  res.status(200).json(products);
+});
+
 app.listen(port, () => {
   console.log(`Il server è inizializzato all'indirizzo ${appUrl}`);
 });
