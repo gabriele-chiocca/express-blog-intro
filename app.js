92ec6aba74f208b2b8bc5576daae051d07fdd4ee
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-27 14:40:07 +0100

    array with image and create the root bacheca

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const app = express();
 const port = 3000;
 const appUrl = `http://localhost:${port}/`;
 
+const products = [
+  {
+    title: 'Ciambella',
+    Img: '/images/ciambellone.jpeg',
+    tags: ['Dolce', 'Ciambella', 'Facile'],
+  },
+  {
+    title: 'Cracked',
+    Img: '/images/cracker_barbabietola.jpeg',
+    tags: ['Cracked', 'Barbabietola', 'Intermedio'],
+  },
+  {
+    title: 'Pane fritto',
+    Img: '/images/pane_fritto_dolce.jpeg',
+    tags: ['Fritto', 'Pane', 'Difficile'],
+  },
+  {
+    title: 'Pasta barbabietola',
+    Img: '/images/pasta_barbabietola.jpeg',
+    tags: ['Pasta', 'Primi piatti', 'Facile'],
+  },
+  {
+    title: 'Torta paeasana',
+    Img: `/images/torta_paesana.jpeg`,
+    tags: ['Dolce', 'Torta', 'Facile'],
+  },
+];
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -15,33 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/bacheca', (req, res) => {
-  const products = [
-    {
-      title: 'Ciambella',
-      Img: 'public\images\ciambellone.jpeg',
-      tags: ['Dolce', 'Ciambella', 'Facile'],
-    },
-    {
-      title: 'Cracked',
-      Img: 'public\images\cracker_barbabietola.jpeg',
-      tags: ['Cracked', 'Barbabietola', 'Intermedio'],
-    },
-    {
-      title: 'Pane fritto',
-      Img: 'public\images\pane_fritto_dolce.jpeg',
-      tags: ['Fritto', 'Pane', 'Difficile'],
-    },
-    {
-      title: 'Pasta barbabietola',
-      Img: 'public\images\pasta_barbabietola.jpeg',
-      tags: ['Pasta', 'Primi piatti', 'Facile'],
-    },
-    {
-      title: 'Torta paeasana',
-      Img: 'public\images\torta_paesana.jpeg',
-      tags: ['Dolce', 'Torta', 'Facile'],
-    },
-  ];
+  products;
 
   res.status(200).json(products);
 });
